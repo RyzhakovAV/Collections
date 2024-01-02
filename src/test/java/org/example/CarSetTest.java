654296b92eba4fa7370a09084402474b0e304b2d
RyzhakovAV <rjizhakov@mail.ru>
2024-01-02 13:04:59 +0500

    HashSet

--- src/test/java/org/example/CarSetTest.java
+++ src/test/java/org/example/CarSetTest.java
@@ -10,30 +10,32 @@ public class CarSetTest {
 
     @Before
     public void setUp() throws Exception {
-        //init
+        carSet = new CarHashSet();
         for (int i = 0; i < 100; i++) {
             carSet.add(new Car("Brand" + i, i));
         }
     }
 
     @Test
-    public void whenAdded100AndNew3SimilarThenSizeBy101() {
+    public void whenAdd3SimilarObjectsThenSizeIncreaseBy1() {
         assertEquals(100, carSet.size());
-        assertTrue(carSet.add(new Car("BMW", 50)));
-        assertFalse(carSet.add(new Car("BMW", 50)));
-        assertFalse(carSet.add(new Car("BMW", 50)));
+        assertTrue(carSet.add(new Car("BMW", 10)));
+        assertFalse(carSet.add(new Car("BMW", 10)));
+        assertFalse(carSet.add(new Car("BMW", 10)));
         assertEquals(101, carSet.size());
     }
 
-    public void whenDeletedThenSize99() {
-        assertTrue(carSet.remove(new Car("Brand50", 50)));
-        assertEquals(99, carSet.size());
-        assertFalse(carSet.remove(new Car("Brand50", 50)));
-        assertEquals(99, carSet.size());
-    }
-
-    public void whenClearThenSize0() {
+    @Test
+    public void whenSetClearedThenSize0() {
         carSet.clear();
         assertEquals(0, carSet.size());
+    }
+
+    @Test
+    public void whenElementRemovedThenSizeDecreased() {
+        assertTrue(carSet.remove(new Car("Brand30", 30)));
+        assertEquals(99, carSet.size());
+        assertFalse(carSet.remove(new Car("Brand30", 30)));
+        assertEquals(99, carSet.size());
     }
 }
